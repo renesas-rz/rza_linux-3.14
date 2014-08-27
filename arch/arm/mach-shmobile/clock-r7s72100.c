783d17bd954f527916248dc613756d5b099eb0a7
Justin Waters <justin.waters@timesys.com>
2014-08-27 00:16:56 -0400

    clock-r7s72100: Add support for USB Clocks

--- arch/arm/mach-shmobile/clock-r7s72100.c
+++ arch/arm/mach-shmobile/clock-r7s72100.c
@@ -155,6 +155,7 @@ enum {
 	MSTP97, MSTP96, MSTP95, MSTP94,
 	MSTP92, MSTP93,
 	MSTP74,
+	MSTP71, MSTP70,
 	MSTP67, MSTP60,
 	MSTP47, MSTP46, MSTP45, MSTP44, MSTP43, MSTP42, MSTP41, MSTP40,
 	MSTP33,	MSTP_NR
@@ -173,6 +174,8 @@ static struct clk mstp_clks[MSTP_NR] = {
 	[MSTP93] = SH_CLK_MSTP8(&bus_clk, STBCR9, 3, 0), /* SPIBSC0 */
 	[MSTP92] = SH_CLK_MSTP8(&bus_clk, STBCR9, 2, 0), /* SPIBSC1 */
 	[MSTP74] = SH_CLK_MSTP8(&peripheral1_clk, STBCR7, 4, 0), /* Ether */
+	[MSTP71] = SH_CLK_MSTP8(&peripheral1_clk, STBCR7, 1, 0), /* USB0 */
+	[MSTP70] = SH_CLK_MSTP8(&peripheral1_clk, STBCR7, 0, 0), /* USB1 */
 	[MSTP67] = SH_CLK_MSTP8(&peripheral1_clk, STBCR6, 7, 0), /* ADC */
 	[MSTP60] = SH_CLK_MSTP8(&r_clk, STBCR6, 0, 0), /* RTC */
 	[MSTP47] = SH_CLK_MSTP8(&peripheral1_clk, STBCR4, 7, 0), /* SCIF0 */
@@ -209,6 +212,8 @@ static struct clk_lookup lookups[] = {
 	CLKDEV_DEV_ID("i2c-riic.2", &mstp_clks[MSTP95]),
 	CLKDEV_DEV_ID("i2c-riic.3", &mstp_clks[MSTP94]),
 	CLKDEV_DEV_ID("r7s72100-ether", &mstp_clks[MSTP74]),
+	CLKDEV_DEV_ID("r8a66597_hcd.0", &mstp_clks[MSTP71]),
+	CLKDEV_DEV_ID("r8a66597_hcd.1", &mstp_clks[MSTP70]),
 	CLKDEV_DEV_ID("sh_adc.0", &mstp_clks[MSTP67]),
 	CLKDEV_DEV_ID("rtc.0", &mstp_clks[MSTP60]),
 	CLKDEV_CON_ID("mtu2_fck", &mstp_clks[MSTP33]),
