1e396f6991e474ffd7175b5bf84649068ccdbf6c
Justin Waters <justin.waters@timesys.com>
2014-09-03 01:22:42 -0400

    clock-r7s72100: Add clocks for USB Gadget

--- arch/arm/mach-shmobile/clock-r7s72100.c
+++ arch/arm/mach-shmobile/clock-r7s72100.c
@@ -230,6 +230,8 @@ static struct clk_lookup lookups[] = {
 	CLKDEV_DEV_ID("sh_adc.0", &mstp_clks[MSTP67]),
 	CLKDEV_DEV_ID("rtc.0", &mstp_clks[MSTP60]),
 	CLKDEV_CON_ID("mtu2_fck", &mstp_clks[MSTP33]),
+	CLKDEV_CON_ID("usb0", &mstp_clks[MSTP71]),
+	CLKDEV_CON_ID("usb1", &mstp_clks[MSTP70]),
 
 	/* ICK */
 	CLKDEV_ICK_ID("sci_fck", "sh-sci.0", &mstp_clks[MSTP47]),
