dbd76c6f03bfc55b994c8cc855f0bb8063bb4707
Justin Waters <justin.waters@timesys.com>
2014-09-24 01:18:33 -0400

    clock-r7s72100: Fix clock name for RTC
    The clock wasn't actually being associated properly, so it was getting
    shut down after the board came up.

--- arch/arm/mach-shmobile/clock-r7s72100.c
+++ arch/arm/mach-shmobile/clock-r7s72100.c
@@ -250,7 +250,7 @@ static struct clk_lookup lookups[] = {
 	CLKDEV_DEV_ID("r8a66597_hcd.0", &mstp_clks[MSTP71]),
 	CLKDEV_DEV_ID("r8a66597_hcd.1", &mstp_clks[MSTP70]),
 	CLKDEV_DEV_ID("sh_adc.0", &mstp_clks[MSTP67]),
-	CLKDEV_DEV_ID("rtc.0", &mstp_clks[MSTP60]),
+	CLKDEV_CON_ID("rtc0", &mstp_clks[MSTP60]),
 	CLKDEV_DEV_ID("ostm.0", &mstp_clks[MSTP51]),
 	CLKDEV_DEV_ID("ostm.1", &mstp_clks[MSTP50]),
 	CLKDEV_CON_ID("mtu2_fck", &mstp_clks[MSTP33]),
